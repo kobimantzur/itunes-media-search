7f13749c12b491c462e2d957cfc3c2e6bb221fe1
kobimantzur <kobimantzur@gmail.com>
2017-11-19 16:57:17 +0200

    Responsive SCSS fixes, adding style variables

--- src/components/Home.js
+++ src/components/Home.js
@@ -21,37 +21,38 @@ class Home extends Component {
       this.props.history.push('/movie/' + movie.trackId);
     })
   }
+  searchKeyPressed(event){
+    if (event.charCode == 13){
+      this.searchArtwork();
+    }
+  }
   renderMovies(){
     if (this.props.searchResultList.length > 0){
         return(
-        <div className="table-responsive">
-        <table className="table">
-          <thead>
-          <tr>
-            <th></th>
-            <th>NAME</th>
-            <th>GENRE</th>
-            <th>PRICE</th>
-            <th></th>
-          </tr>
-          </thead>
-          <tbody>
+        <div className="table results-table">
+          <div className="head">
+            <div className="table-row">
+              <div className="cell">NAME</div>
+              <div className="cell">GENRE</div>
+              <div className="cell">PRICE</div>
+              <div className="cell"></div>
+            </div>
+          </div>
+          <div className="body">
         {this.props.searchResultList.map((movie, index) => {
             return (
-                <tr key={index} className="movie">
-                <td className="index"><h4>{index + 1}</h4></td>
-                <td className="name">
-                  <img src={movie.artworkUrl60} />
-                  <span>{movie.trackCensoredName}</span>
-                </td>
-                <td className="genre"> {movie.primaryGenreName}</td>
-                <td className="price"><span>{movie.trackPrice} </span><small>{movie.currency}</small></td>
-                <td className="read-more"><button className="btn btn-primary" onClick={() => this.readMoreClicked(movie)}>Read More</button></td>
-                </tr>
+                <div key={index} className="table-row movie">
+                <div className="col-xs-12 cell name">
+                  <img className="col-xs-3" src={movie.artworkUrl60} />
+                  <span className="col-xs-9">{movie.trackCensoredName}</span>
+                </div>
+                <div className="cell genre"><b>Genre </b> {movie.primaryGenreName}</div>
+                <div className="cell price"><b>Price </b> <span>{movie.trackPrice} </span><small>{movie.currency}</small></div>
+                <div className="cell read-more"><button className="btn btn-primary" onClick={() => this.readMoreClicked(movie)}>Read More</button></div>
+                </div>
             )
           })}
-          </tbody>
-        </table>
+          </div>
         </div>
     )
     }
@@ -78,7 +79,7 @@ class Home extends Component {
         <h1>Itunes Movies Search</h1>
         <div className="search">
               <div className="input-group col-md-12">
-                  <input value={this.props.term} onChange={this.termChanged.bind(this)} type="text" className="form-control input-lg" placeholder="Let's search a movie" />
+                  <input value={this.props.term} onKeyPress={this.searchKeyPressed.bind(this)} onChange={this.termChanged.bind(this)} type="text" className="form-control input-lg" placeholder="Let's search a movie" />
                   <span className="input-group-btn">
                       <button className="btn btn-info btn-lg" type="button" onClick={() => {this.searchArtwork()}}>
                           <i className="glyphicon glyphicon-search"></i>
